9e3da4ea3d76a9d208525ccc79bd93c4851328c1
Denzel Moran Fortuno <denzelfortunomoran@gmail.com>
2025-12-30 08:31:06 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Computer Science & Technology", "Information Systems & Software Engineering", "Strategy & Leadership"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
